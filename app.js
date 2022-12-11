c81c4968a16a002e89888e8af4d3a5fb7717f371
Allyson Camp <Allysoncamp92@gmail.com>
2022-12-10 16:36:14 -0800

    created nametag

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-// grab DOM elements
+// const booger = document.getElementById('nametag');
+// booger.style.backgroundColor = 'purple';
 
-// set event listeners
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const button = document.getElementById('update-button');
+button.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    console.log(nameDisplay);
+    // nameDisplay.textContent = 'Julie';
+    nameDisplay.textContent = nameInput.value;
+    nameDisplay.style.color = 'red';
+});
+
+const pronounDisplay = document.getElementById('pronoun-display');
+const pronounInput = document.getElementById('pronoun-input');
+const button2 = document.getElementById('update-button');
+button.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    console.log(pronounDisplay);
+    pronounDisplay.textContent = pronounInput.value;
+    pronounDisplay.style.color = 'blue';
+});
